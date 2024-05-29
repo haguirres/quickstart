67f39d9b4a71f4813ae682d848103471a6f6215d
Hector Aguirre <eduardo.aguirre@outlook.com>
2024-05-29 11:19:23 -0600

    adding nat gateway creation

--- __main__.py
+++ __main__.py
@@ -1,7 +1,7 @@
 """An Azure RM Python Pulumi program"""
 
 import pulumi
-from networking import virtual_network, network_security_group, subnet
+from networking import virtual_network, network_security_group, subnet, nat_gateway
 from resources import resource_group
 
 # Common variables
@@ -22,10 +22,33 @@ flow_timeout_in_minutes = 4
 enable_ddos_protection = False
 encryption = {"enabled": False, "enforcement": None}
 enable_vm_protection = False
+
+# NAT Gateway variables
+nat_gateway_name = "demoNATGateway"
+idle_timeout_in_minutess = 10
+public_ip_addresses = []
+zones = None
+# Resource creation
+resource_group_instance = resource_group.create_resource_group(
+    resource_group_name,
+    location,
+    tags)
+
+# Create NAT Gateway
+nat_gateway_instance = nat_gateway.create_nat_gateway(
+    resource_group_instance.name, nat_gateway_name, location, public_ip_addresses, idle_timeout_in_minutess, tags,
+    zones)
+
+# Virtual Network creation
+virtual_network_instance = virtual_network.create_virtual_network(
+    resource_group_instance.name, location, tags, vnet_name, address_space, dns_servers, flow_timeout_in_minutes, encryption, enable_vm_protection, enable_ddos_protection)
+
+# Create NSGs and subnets
 subnets = [
     {
         "name": "BastionSubnet",
         "addressPrefix": "10.0.1.0/24",
+        "useNatGateway": False,
         "nsg": {
             "name": "BastionNSG",
             "security_rules": [
@@ -57,6 +80,7 @@ subnets = [
     {
         "name": "AppGatewaySubnet",
         "addressPrefix": "10.0.2.0/24",
+        "useNatGateway": False,
         "nsg": {
             "name": "AppGatewayNSG",
             "security_rules": [
@@ -77,6 +101,7 @@ subnets = [
     {
         "name": "PrivateSubnet",
         "addressPrefix": "10.0.3.0/24",
+        "useNatGateway": True,
         "nsg": {
             "name": "PrivateNSG",
             "security_rules": [
@@ -130,6 +155,7 @@ subnets = [
     {
         "name": "PublicSubnet",
         "addressPrefix": "10.0.4.0/24",
+        "useNatGateway": False,
         "nsg": {
             "name": "PublicNSG",
             "security_rules": [
@@ -182,17 +208,6 @@ subnets = [
     }
 ]
 
-# Resource creation
-resource_group_instance = resource_group.create_resource_group(
-    resource_group_name,
-    location,
-    tags)
-
-# Virtual Network creation
-virtual_network_instance = virtual_network.create_virtual_network(
-    resource_group_instance.name, location, tags, vnet_name, address_space, dns_servers, flow_timeout_in_minutes, encryption, enable_vm_protection, enable_ddos_protection)
-
-# Create NSGs and subnets
 subnet_instances = []
 nsg_instances = []
 
@@ -211,12 +226,16 @@ for subnet_config in subnets:
         resource_group_instance.name,
         subnet_config["name"],
         subnet_config["addressPrefix"],
-        nsg_instance
+        nsg_instance,
+        [],
+        nat_gateway_instance if subnet_config["useNatGateway"] else None
     )
     subnet_instances.append(subnet_instace)
+
 
 # Export IDs
 pulumi.export("RG_ID", resource_group_instance.id)
 pulumi.export("VNET_ID", virtual_network_instance.id)
+pulumi.export("NAT_GATEWAY_ID", nat_gateway_instance.id)
 pulumi.export("NSG_IDs", [nsg.id for nsg in nsg_instances])
 pulumi.export("SUBNET_IDs", [subnet.id for subnet in subnet_instances])
